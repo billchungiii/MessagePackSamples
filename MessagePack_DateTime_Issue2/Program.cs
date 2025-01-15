581d69e934c2e5feb2bdc17506e4f00197011a2f
Bill Chung <bill_chungiii@hotmail.com>
2025-01-16 02:03:08 +0800

    Modify MessagePack_DateTime_Issue2

--- MessagePack_DateTime_Issue2/Program.cs
+++ MessagePack_DateTime_Issue2/Program.cs
@@ -12,11 +12,24 @@ namespace MessagePack_DateTime_Issue2
                 BirthDay = new DateTime(1985, 3, 4, 0, 0, 0)
             };
 
-            byte[] bytes = MessagePackSerializer.Serialize(person);
-            Person personDeserialized = MessagePackSerializer.Deserialize<Person>(bytes);
+            Console.WriteLine("from Unspecified");
+            SerializeAndDeserializePerson(person);
+            Console.WriteLine();
+            person.BirthDay = new DateTime(1985, 3, 5, 0, 0, 0, DateTimeKind.Utc);
+            Console.WriteLine("from Utc");
+            SerializeAndDeserializePerson(person);
+            Console.WriteLine();
+            person.BirthDay = new DateTime(1985, 3, 6, 0, 0, 0, DateTimeKind.Local);
+            Console.WriteLine("from Local");
+            SerializeAndDeserializePerson(person);
+        }
+
+        private static void SerializeAndDeserializePerson(Person person)
+        {
+            var bytes = MessagePackSerializer.Serialize(person);
+            var personDeserialized = MessagePackSerializer.Deserialize<Person>(bytes);
             DisplayResult("person", person.BirthDay);
             DisplayResult("personDeserialized", personDeserialized.BirthDay);
-
         }
 
         private static void DisplayResult(string prefix, DateTime value)
